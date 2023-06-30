4c4e3d5f45f60dca002a2a2e716eb8eedf3fce92
dan046 <dandarellreyes@gmail.com>
2023-06-30 14:35:58 +0800

    implemented changes, disabled text selection

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 const Card = ({ children }: Props) => {
   return (
-    <main className="w-full overflow-hidden rounded-2xl bg-white shadow-lg md:w-[720px]">
+    <main className="w-full select-none overflow-hidden rounded-2xl bg-white shadow-lg md:w-[720px]">
       {children}
     </main>
   )
